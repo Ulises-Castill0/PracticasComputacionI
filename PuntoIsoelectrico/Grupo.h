e020ec2d7063327a76a8d5c4109e95f3c18ab347
Ulises-Castill0 <71796775+Ulises-Castill0@users.noreply.github.com>
2020-11-26 19:45:43 -0600

    Create Grupo.h

---
+++ PuntoIsoelectrico/Grupo.h
@@ -0,0 +1,28 @@
+
+#ifndef PUNTOISOELECTRICO_GRUPO_H
+#define PUNTOISOELECTRICO_GRUPO_H
+
+#include <string>
+using namespace std;
+
+class Grupo{
+
+public:
+    Grupo()
+    {
+        nombreGrupo = "";
+        pkGrupo = 0;
+        cargaGrupo = 0;
+    }
+    Grupo(string nombre, float pk, int carga);
+    float getpK();
+    int getCarga();
+
+private:
+    string nombreGrupo;
+    float pkGrupo;
+    int cargaGrupo;
+
+};
+
+#endif
