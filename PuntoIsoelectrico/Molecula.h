8739b497dd18f25427f1e2da25dfd5bb44dc9066
Ulises-Castill0 <71796775+Ulises-Castill0@users.noreply.github.com>
2020-11-26 19:47:57 -0600

    Create Molecula.h

---
+++ PuntoIsoelectrico/Molecula.h
@@ -0,0 +1,17 @@
+
+#ifndef PUNTOISOELECTRICO_MOLECULA_H
+#define PUNTOISOELECTRICO_MOLECULA_H
+
+#include <string>
+#include "Grupo.h"
+using namespace std;
+
+class Molecula{
+
+public:
+
+private:
+
+};
+
+#endif
